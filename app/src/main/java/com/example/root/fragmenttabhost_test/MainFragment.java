4fb58f86bb8f3e3fdc7b0ffb6dec09f665a970f6
xuezhit <xuezhit@opera.com>
2016-12-12 21:21:04 +0800

    third commit

---
+++ app/src/main/java/com/example/root/fragmenttabhost_test/MainFragment.java
@@ -0,0 +1,29 @@
+package com.example.root.fragmenttabhost_test;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 16-11-21.
+ */
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.main_fragment,null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+}
